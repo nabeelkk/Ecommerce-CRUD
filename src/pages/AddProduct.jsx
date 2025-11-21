d5d78df3d4e2e9dc61c8910b70bbb44088e1068a
nabeelkk <mohammmednabeelkk.tkl@gmail.com>
2025-11-21 14:24:48 +0530

    form validation

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -17,7 +17,12 @@ export default function AddProduct() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(form.title === "" || form.price ==""|| form.category === "" || form.image === "" || form.description === ""){
+        toast.error("All fields are required!");
+        return
+    }
     await addProduct({ ...form, price: Number(form.price) });
+    
     toast.success("Product added successfully!");
     nav("/");
   };
@@ -28,27 +33,32 @@ export default function AddProduct() {
         placeholder="Title"
         className="input input-bordered w-full"
         onChange={(e) => setForm({ ...form, title: e.target.value })}
+        // required
       />
       <input
         placeholder="Price"
         type="number"
         className="input input-bordered w-full"
         onChange={(e) => setForm({ ...form, price: e.target.value })}
+        // required
       />
       <input
         placeholder="Image URL"
         className="input input-bordered w-full"
         onChange={(e) => setForm({ ...form, image: e.target.value })}
+        // required
       />
       <input
         placeholder="Category"
         className="input input-bordered w-full"
         onChange={(e) => setForm({ ...form, category: e.target.value })}
+        // required
       />
       <textarea
         placeholder="Description"
         className="textarea textarea-bordered w-full"
         onChange={(e) => setForm({ ...form, description: e.target.value })}
+        // required
       />
 
       <button className="btn btn-primary w-full">Add Product</button>
